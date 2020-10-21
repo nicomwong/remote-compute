222d5bb54b86eccb15f4c1d5b0cef75d361d178c
Nicholas Wong <nicowong8@gmail.com>
2020-10-21 16:16:26 -0700

    initial commit, added tcp

---
+++ tcp/client_python_tcp.py
@@ -0,0 +1,47 @@
+# client_python_tcp.py
+
+# UCSB CS 176A Fall 2020 HW #1 Socket Programming
+# Nicholas M Wong, PID: 3018439
+# Syntax/libraries learned from geeksforgeeks.org, w3schools.com, docs.python.org
+
+import socket
+import sys
+
+# Establish a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # AF_INET for ipv4 address family, SOCK_STREAM means TCP protocol
+
+# # Get user input for parameters
+# host = input("Enter sever name or IP address: ")
+# port = int(input("Enter port: "))
+# if not(0 <= port <= 65535):
+#     print("Invalid port number")
+    
+# command = input("Enter command: ")
+
+
+# FORCED FOR TESTING
+host = "localhost"
+port = 12345
+command = "ls"
+
+# Try connecting to the server
+try:
+    s.connect( (host, port) )
+# If socket.error is caught, then print an error
+except socket.error as err:
+    print("Could not connect to server.")
+    sys.exit()
+
+# Send command to the server
+s.send( command.encode() )
+
+# Receive the data (stdout from the command) from the server
+dataFromServer = s.recv(1024)
+
+# Store its contents in a file
+f = open("client_tcp_stdout.txt", "w")
+f.write( dataFromServer.decode() )
+f.close()
+
+# Close the connection
+s.close()
---
+++ tcp/server_python_tcp.py
@@ -0,0 +1,48 @@
+# server_python_tcp.py
+
+# UCSB CS 176A Fall 2020 HW #1 Socket Programming
+# Nicholas M Wong, PID: 3018439
+# Syntax/libraries learned from geeksforgeeks.org, w3schools.com, docs.python.org
+
+import socket
+import sys
+import subprocess
+
+# Create the socket
+s = socket.socket()
+
+# Set the port
+port = int(sys.argv[1])
+if not(0 <= port <= 65535):
+    print("Invalid port number")
+
+# Bind to this machine with the port
+s.bind( ('', port) )
+
+# Put the socket into listening mode
+s.listen(5)
+
+# Infinite loop to wait for client connection
+while True:
+    
+    # Wait until a connection is received (blocking)
+    c, addr = s.accept()
+
+    # Receive the data sent
+    commandFromClient = c.recv(1024)
+
+    # Run the command on the shell from server and store the output
+    stdout = subprocess.check_output( commandFromClient.decode(), shell=True )
+
+    # Put the output into a file
+    f = open("server_tcp_stdout.txt", "w")
+    f.write( stdout.decode() )
+    f.close()
+
+    # Send the file contents back to the client
+    f = open("stdoutOnServer.txt", "r")
+    c.send( f.read().encode() )
+    f.close()
+
+    # Close the connection
+    c.close()
