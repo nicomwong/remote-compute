3d9f29dd498f073d7bacaec71e9e6e91df11ba23
Nicholas Wong <nicowong8@gmail.com>
2020-10-22 22:44:34 -0700

    Added case for commands of the format (cmd > file)

--- tcp/server_python_tcp.py
+++ tcp/server_python_tcp.py
@@ -18,6 +18,8 @@ port = int(sys.argv[1])
 if not(0 <= port <= 65535):
     print("Invalid port number")
 
+fileName = "server_tcp_stdout.txt"
+
 # Create the socket
 s = socket.socket()
 
@@ -35,23 +37,45 @@ while True:
 
     # Try to receive the command
     try:
-        commandFromClient = c.recv(1024)
+        cmdFromClient = c.recv(1024)
     # If it times out, print an error and ditch the message
     except socket.timeout:
         print("Failed to receive instructions from the client.")
         break
-    # Else, process the received commandFromClient
+    # Else, process the received cmdFromClient
     else:
-        # Run the command on the shell from server and store the output
-        stdout = subprocess.check_output( commandFromClient.decode(), shell=True )
 
-        # Put the output into a file
-        f = open("server_tcp_stdout.txt", "w")
-        f.write( stdout.decode() )
+        # Store command as str
+        cmdStr = cmdFromClient.decode()
+
+        notStdOut = False
+        cmdList = []
+        # Determine if the command format is of 'cmd > file'
+        if '>' in cmdStr:
+            notStdOut = True
+            cmdList = cmdStr.split('>')
+            fileSpecified = cmdList[1][1:]  # File name to the right of '>' w/o the first space
+        
+        # Run the command on the shell from server and store the output
+        stdout = subprocess.check_output( cmdFromClient.decode(), shell=True )
+
+        # Open the server file to write to
+        f = open(fileName, "w")
+
+        # If the output is in stdout, then write from stdout
+        if notStdOut == False:
+            f.write( stdout.decode() )
+        # Else, write from the specified file
+        else:
+            fTemp = open(fileSpecified, "r")
+            f.write( fTemp.read() )
+            fTemp.close()
+
+        # Close the file
         f.close()
 
         # Send the file contents back to the client
-        f = open("server_tcp_stdout.txt", "r")
+        f = open(fileName, "r")
         c.send( f.read().encode() )
         f.close()
 
